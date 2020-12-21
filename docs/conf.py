0c3ab34d4d8afa40a9723b06a02874679af79abb
brendan <2bndy5@gmail.com>
2020-12-21 10:43:10 -0800

    fix conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -12,12 +12,12 @@
 #
 import os
 import sys
+import subprocess
 import recommonmark
 from recommonmark.transform import AutoStructify
 from recommonmark.parser import CommonMarkParser
 
 sys.path.insert(0, os.path.abspath("."))
-
 
 # -- Project information -----------------------------------------------------
 
